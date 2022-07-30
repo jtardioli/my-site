eebac1c694a05f0cd8bc3efb39c045737e816fa2
jtardioli <joshua.tardioli@gmail.com>
2022-07-30 19:48:37 -0400

    first commit

---
+++ src/components/LinkButton.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const LinkButton = ({ img, link }: { img: string; link: string }) => {
+  return (
+    <a href={link} target="_blank" rel="noreferrer">
+      <div className={styles.modal_button}>
+        <Image src={img} alt="logo" width={150} height={150} />
+      </div>
+    </a>
+  );
+};
+
+export default LinkButton;
